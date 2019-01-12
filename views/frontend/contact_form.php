53e4b0b09d6b3a4e2153dec0cad6166346bb1354
Zylkao <rfrancois1993@gmail.com>
2019-01-12 16:09:39 +0100

    champ required
    champ required

--- views/frontend/contact_form.php
+++ views/frontend/contact_form.php
@@ -1,13 +1,13 @@
 <div id="contact_form">
   <form action="submit" method="post">
     <label for="pseudo"> Identifiant </label><br />
-    <input type="text" name="pseudo" /><br />
+    <input type="text" name="pseudo" required/><br />
 
     <label for="email"> E-mail </label><br />
-    <input type="email" name="email" /><br />
+    <input type="email" name="email" required/><br />
 
     <label for="message"> Votre Message </label><br />
-    <textarea id="contact_message" name="message" /></textarea><br />
+    <textarea id="contact_message" name="message" required /></textarea><br />
 
     <input class="button" type="submit" value="Valider" />
     <a class="button" href='index.php'/> Annuler </a>
